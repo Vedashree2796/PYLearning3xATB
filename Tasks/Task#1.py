ae4c16eb39cfdeea59a699649c28346c7b725761
Vedashree <vedashree1604@gmail.com>
2024-06-11 18:22:17 +0530

    Tasks10062024

---
+++ Tasks/Task#1.py
@@ -0,0 +1,16 @@
+# Expalin the diff Between = operator & == operator in python
+# = is a assignment operators, for assigning a value to a variable
+# == is a relationship between two operands
+# ex x = 2 _> 2 is assign to the variable x, 4==10 checks whether the relation is equal or not
+
+
+# what does the ** operator do in python and how is it used?
+# ** is a exponentiation operator, it is used to raise the first operand to power of second
+x = 3
+y = 2
+print(x**y)
+
+
+# what does the ^ operator do in python and what contex is it commonly used?
+# It is Bitwise XOR operator
+# it compares each bit of two numbers
---
+++ Tasks/Task#2.py
@@ -0,0 +1,13 @@
+# Python script that calculate the square and cube of the given number
+number = float(input("Enter your number"))
+square = number ** 2
+cube = number ** 3
+print("square of", number, "is:", square)
+print("cube of", number, "is", cube)
+
+
+
+# Create a program that takes two numbers as input & prints whether the first number is greater than, less than
+number1 = 60
+number2 = 40
+print("number1 is greater" if number1 > number2 else "number1 is lesser" and "equal to number1")
