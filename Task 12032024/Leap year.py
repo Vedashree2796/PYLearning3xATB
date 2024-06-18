81f94abae707ba7d2970b64234adf75e51b0a8a5
Vedashree2796 <vedashree1604@gmail.com>
2024-06-18 19:45:55 +0530

    Leap year.py

--- Task 12032024/Leap year.py
+++ Task 12032024/Leap year.py
@@ -3,7 +3,11 @@
 
 year = 2024
 if (year%4 == 0):
-   print("Leap year")
+   (year % 100 != 0)
+   (year % 400 == 0)
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("Leap Year")
 else:
     print("not leap year")
 
