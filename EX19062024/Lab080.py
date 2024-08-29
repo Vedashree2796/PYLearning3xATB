013cf1bdfccd9bedd36ed796fb358c331c92a979
Vedashree <vedashree1604@gmail.com>
2024-08-29 22:16:47 +0530

    Functions Scope

---
+++ EX19062024/Lab080.py
@@ -0,0 +1,14 @@
+# Tuple - Collection of Items
+
+my_list = [1, 2, 3, 4, 5]
+print(id(my_list))
+my_list[0] = 21 # Mutable the values can be changeable
+print(my_list)
+print(id(my_list))
+
+
+
+
+my_tuple = (1, 2, 3, 4, 5,5)
+#my_tuple[0] = 21 # Immutable   the values are not changeable
+print(my_tuple)
