b24669fe8a8162db90a9f3a26e6b1d24d599f795
Vedashree <vedashree1604@gmail.com>
2024-09-12 20:07:04 +0530

    Exception handling

---
+++ JULY2024/Ex03072024/Lab149.py
@@ -0,0 +1,18 @@
+a = int(input("Ent the num1")) #ValueError: invalid literal
+b = int(input("Ent the num2"))
+c = a/b # ZeroDivisionError
+print("Result Div is" ,c)
+
+#What are the different problem we can face?
+
+# why we want to handle this?
+
+#ATM -> SBI
+#ATM -> SBI -> INSERTED card -> 10k
+#Enter the amount, PIN, money got deducted but you didn't get it
+#via ATM, There is not messsage(not handled properly)
+
+
+#Bad User Exp
+#User will curse you :)
+#We need to handle it and give user a better exp.
